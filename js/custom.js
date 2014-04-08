ce9d82e56d9836e48a46823f2f2055f674601309
Aziz <shift_s@mail.ru>
2014-04-08 17:19:51 +0400

    soon final stage

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var speed = 200;
+
 	var socialList = $('.menu-social-list'),
 		socialListItem = socialList.find('li');
 	socialListItem.on('mouseover', function() {
@@ -7,19 +9,44 @@ $(document).ready(function() {
 			linkHover = $(this).find('a').last();
 		linkPassive.stop().animate({
 			top: -40
-		});
+		}, speed);
 		linkHover.stop().animate({
 			top: 0
-		});
+		}, speed);
 	}).on('mouseleave', function() {
 		var $this = $(this),
 			linkPassive = $(this).find('a').first(),
 			linkHover = $(this).find('a').last();
 		linkPassive.stop().animate({
 			top: 0
-		});
+		}, speed);
 		linkHover.stop().animate({
 			top: 40
-		});
+		}, speed);
 	});
+
+
+	var currentPage = $('.current-page'),
+		navLinksList = $('.menu-list'),
+		navLinksLi = navLinksList.find('a'),
+		currentLink = $('.current-page-link'),
+		widthCurrentPage = currentLink.outerWidth() - 1,
+		leftCurrentLink = currentLink.offset().left - navLinksList.offset().left + 1;
+	currentPage.css({
+		width: widthCurrentPage
+	});
+	navLinksLi.on('mouseover', function() {
+	var $this = $(this),
+		newWidthCurrentPage = $this.outerWidth(),
+		leftCurrentPage = $this.offset().left - navLinksList.offset().left + 1;
+	currentPage.stop().animate({
+		left: leftCurrentPage,
+		width: newWidthCurrentPage
+	}, speed);
+	}).on('mouseleave', function() {
+	currentPage.stop().animate({
+		left: leftCurrentLink,
+		width: widthCurrentPage
+	}, speed);
+});
 });
